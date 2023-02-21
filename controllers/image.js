5aba9587c45bca7a6b3b0ff28fca1010d4589d1e
stpeteriz <st.peteriz17@gmail.com>
2023-02-21 19:32:24 +0100

    adding files

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+    apiKey: 'aa63fc0dcfcc4b54b77e0354ffda44bb'
+  });
+
+export const handleApiCall = (req,res) =>{
+    app.models.predict(
+        {
+          id: 'face-detection',
+          name: 'face-detection',
+          version: '6dc7e46bc9124c5c8824be4822abe105',
+          type: 'visual-detector',
+        }, req.body.input)
+        .then(data =>{
+            res.json(data)
+        })
+        .catch(err =>res.status(400).json('Unable to work with API'))
+}
+  
+
+export const handleImagePut = (req,res,db) =>{
+    const { id }= req.body;
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning('entries').then(entries =>{
+        res.json(entries[0].entries)
+    }).catch(err => res.status(400).json('unable to get entries'))
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+import  express  from "express";
+import bcrypt from "bcrypt";
+import cors from "cors";
+import knex from "knex";
+
+import {handleRegisterPost} from "./controllers/register.js";
+import {handleSignInPost} from "./controllers/signin.js";
+import { handleProfileGet } from "./controllers/profile.js";
+import { handleApiCall, handleImagePut } from "./controllers/image.js";
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : 'containers-us-west-33.railway.app',
+      user : 'postgres',
+      password : '5ulqvU87y9nKJGe3lv6L',
+      database : 'railway',
+      port: 5686
+    }
+});
+
+db.select('*').from('users').then(data => {
+    console.log(data)
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req,res) =>{
+    res.json('It is running');
+})
+app.post('/signin', (req, res) => { handleSignInPost(req, res, db, bcrypt) })
+app.post('/register', (req, res) => { handleRegisterPost(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => {handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => {handleImagePut(req, res, db)})
+app.post('/imageurl', (req, res) => {handleApiCall(req, res)})
+
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log(`app is running on port ${process.env.PORT}`)
+})
+
+
+/* Routes
+/--> res = this is working
+/singin ---> POST request | respond = succes/fail
+/register ---> POST req   | respond = user
+/profile/:userId --> GET  | respond = user
+/image --> PUT updating user info
+
+
+
+
+*/
